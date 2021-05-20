291ffba74fffcefac314471e5a8c065a68eb88c0
John Jirik <jjirik@gmail.com>
2021-05-19 23:26:46 -0400

    On hold while I solve a problem...

--- object.php
+++ object.php
@@ -14,6 +14,41 @@ $object = array(
     'loaded' =>'',
 );
 
+// Database connection credentials
+$servername = 'localhost';
+$username = 'homestead';
+$password = 'secret';
+
+// Create connection
+$connection = new mysqli($servername, $username, $password);
+
+// Check for a connection error
+if ($connection->connect_error) {
+    echo 'Connection failed: ' , $connection->connect_error;
+    exit;
+}
+
+// Otherwise, connect successfully!
+echo 'Connected successfully!';
+
+/* from here to the close of comment I've blocked until I work out the problem in line 39
+// Connect to the "fitl" database
+$connection->select_db('fitl')
+
+// Query to select the object
+$sql = 'SELECT * FROM newsarchive WHERE id = ' . $id;
+
+// Execute the query
+$result = $connection->query($sql);
+
+// Check for and retrieve the object
+if ($result->num_rows > 0) {
+    $object = $result->fetch_assoc();
+    echo '<pre>';
+    print_r($object);
+    echo '</pre>';
+}
+*/
 // set the object variables based on the id value from the URL
 // if id is 1
 if ($id == 1) {
